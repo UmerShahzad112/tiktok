bbf047880cd7f24511150b2d02513a07641ce495
UmerShahzad112 <159061143+UmerShahzad112@users.noreply.github.com>
2024-06-29 12:40:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import streamlit as st
-import os, sys
-
-@st.cache_resource
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-
-_ = installff()
+import os
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
+from selenium.webdriver.firefox.options import Options
+
+@st.experimental_singleton
+def install_geckodriver():
+    os.system('sbase install geckodriver')
+    os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = install_geckodriver()
+
+opts = Options()
 opts.add_argument("--headless")
 browser = webdriver.Firefox(options=opts)
 
-browser.get('https://www.google.com/')
+browser.get('http://example.com')
 st.write(browser.page_source)
